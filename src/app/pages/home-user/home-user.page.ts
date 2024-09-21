e1483191958db65c10ac982db0f5bd5f6bf75f11
Niko Kacic <niko.kacic@gmail.com>
2024-09-21 01:20:06 -0300

    Creación de servicio para los ramos y uso del activaterouter para su render de cada uno

--- src/app/pages/home-user/home-user.page.ts
+++ src/app/pages/home-user/home-user.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Auth } from '@angular/fire/auth';
 import { User } from 'firebase/auth';
+import {CourseServiceService} from './course-service.service'
 
 
 @Component({
@@ -10,50 +11,11 @@ import { User } from 'firebase/auth';
 })
 export class HomeUserPage implements OnInit {
 
-  public courses = [
-      {
-         id: '1',
-         title: 'Arquitectura de Software',
-         imageURL: 'https://cdn-icons-png.flaticon.com/512/5271/5271007.png',
-         details: ['Cursando |', 'ERNESTO LEONIDAS VELASQUEZ VELASQUEZ |', 'Más información'],
-      },
-      {
-         id: '2',
-         title: 'Calidad de Software',
-         imageURL: 'https://cdn-icons-png.flaticon.com/512/4677/4677559.png',
-         details: ['Cursando |', 'DANIEL ENRIQUE RIQUELME RIGOT |', 'Más información'],
-      },
-      {
-         id: '3',
-         title: 'Estadistica Descriptiva ',
-         imageURL: 'https://cdn-icons-png.flaticon.com/512/432/432796.png',
-         details: ['Cursando |', 'MONICA NATHALIA PANES MARTINEZ |', 'Más información'],
-      },
-      {
-         id: '4',
-         title: 'Etica Laboral',
-         imageURL: 'https://cdn-icons-png.flaticon.com/512/5776/5776927.png',
-         details: ['Cursando |', 'LEONARDO OSVALDO MUÑOZ VILLALÓN |', 'Más información'],
-      },
-      {
-         id: '5',
-         title: 'Inlges Intermedio',
-         imageURL: 'https://images.vexels.com/content/201997/preview/english-book-flat-17d09a.png',
-         details: ['Cursando |', 'JOSE SANTOS JARA FUENTES |', 'Más información'],
-      },
-      {
-         id: '6',
-         title: 'Programación Aplicaciones Moviles',
-         imageURL: 'https://www.svgrepo.com/show/530452/mobile-app.svg',
-         details: ['Cursando |', 'CARLOS FERNANDO MARTINEZ SANCHEZ|', 'Más información']
-      },
-
-  ];
-
 
   email: string | null = null;
+  courses: any[] = [];
 
-  constructor(private auth: Auth) {
+  constructor(private auth: Auth , private courseService: CourseServiceService) {
 
    }
 
@@ -63,6 +25,8 @@ export class HomeUserPage implements OnInit {
     if (user) {
       this.email = user.email;
     }
+
+    this.courses = this.courseService.getCourses();
 
   }
 
