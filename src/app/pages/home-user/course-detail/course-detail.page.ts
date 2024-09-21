1f07e20a01e8fadb9e9523111d703f972a809e9a
Niko Kacic <niko.kacic@gmail.com>
2024-09-21 01:23:09 -0300

    disclaimer: hago este commit para el cambio del titulo de los ramos, ya que el anterior nacio de la frustración hasta que resulto el enrutamiento

---
+++ src/app/pages/home-user/course-detail/course-detail.page.ts
@@ -0,0 +1,30 @@
+import { ActivatedRoute } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { CourseServiceService } from '../course-service.service';
+
+
+@Component({
+  selector: 'app-course-detail',
+  templateUrl: './course-detail.page.html',
+  styleUrls: ['./course-detail.page.scss'],
+})
+export class CourseDetailPage implements OnInit {
+
+  course:any;
+
+  constructor(private activatedrouter: ActivatedRoute, private coursesdetail: CourseServiceService) { }
+
+  ngOnInit() {
+    this.activatedrouter.paramMap.subscribe(paramMap =>{
+      //TODO se creara futura validación
+
+      const recipedId = paramMap.get('placeId');
+      console.log(recipedId)
+
+      this.course = this.coursesdetail.getCourse(recipedId);
+      console.log(this.course)
+    })
+
+  }
+
+}
