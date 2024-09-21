6e1e9f23957a50118bf8b42da368633837fccf73
Niko Kacic <niko.kacic@gmail.com>
2024-09-21 20:38:26 -0300

    Pages para cada curso creado, asi como agregar el avatar de cada profesor

--- src/app/pages/home-user/course-service.service.ts
+++ src/app/pages/home-user/course-service.service.ts
@@ -10,37 +10,44 @@ export class CourseServiceService {
        id: '1',
        title: 'Arquitectura de Software',
        imageURL: 'https://cdn-icons-png.flaticon.com/512/5271/5271007.png',
-       details: ['Cursando |', 'ERNESTO LEONIDAS VELASQUEZ VELASQUEZ |', 'Más información'],
+       profileImg: 'https://avatars.githubusercontent.com/u/34869177?v=4',
+       details: ['Cursando ', 'ERNESTO LEONIDAS VELASQUEZ VELASQUEZ ', 'Más información'],
+
     },
     {
        id: '2',
        title: 'Calidad de Software',
        imageURL: 'https://cdn-icons-png.flaticon.com/512/4677/4677559.png',
-       details: ['Cursando |', 'DANIEL ENRIQUE RIQUELME RIGOT |', 'Más información'],
+       profileImg: 'https://learn.content.blackboardcdn.com/3900.100.0-rel.32+b7093a7/images/ci/ng/default_profile_avatar.svg',
+       details: ['Cursando ', 'DANIEL ENRIQUE RIQUELME RIGOT ', 'Más información'],
     },
     {
        id: '3',
        title: 'Estadistica Descriptiva ',
        imageURL: 'https://cdn-icons-png.flaticon.com/512/432/432796.png',
-       details: ['Cursando |', 'MONICA NATHALIA PANES MARTINEZ |', 'Más información'],
+       profileImg: 'https://learn.content.blackboardcdn.com/3900.100.0-rel.32+b7093a7/images/ci/ng/default_profile_avatar.svg',
+       details: ['Cursando ', 'MONICA NATHALIA PANES MARTINEZ ', 'Más información'],
     },
     {
        id: '4',
        title: 'Etica Laboral',
        imageURL: 'https://cdn-icons-png.flaticon.com/512/5776/5776927.png',
-       details: ['Cursando |', 'LEONARDO OSVALDO MUÑOZ VILLALÓN |', 'Más información'],
+       profileImg: 'https://learn.content.blackboardcdn.com/3900.100.0-rel.32+b7093a7/images/ci/ng/default_profile_avatar.svg',
+       details: ['Cursando ', 'LEONARDO OSVALDO MUÑOZ VILLALÓN ', 'Más información'],
     },
     {
        id: '5',
        title: 'Inlges Intermedio',
        imageURL: 'https://images.vexels.com/content/201997/preview/english-book-flat-17d09a.png',
-       details: ['Cursando |', 'JOSE SANTOS JARA FUENTES |', 'Más información'],
+       profileImg: 'https://learn.content.blackboardcdn.com/3900.100.0-rel.32+b7093a7/images/ci/ng/default_profile_avatar.svg',
+       details: ['Cursando ', 'JOSE SANTOS JARA FUENTES ', 'Más información'],
     },
     {
        id: '6',
        title: 'Programación Aplicaciones Moviles',
        imageURL: 'https://www.svgrepo.com/show/530452/mobile-app.svg',
-       details: ['Cursando |', 'CARLOS FERNANDO MARTINEZ SANCHEZ|', 'Más información']
+       profileImg: 'https://avatars.githubusercontent.com/u/8823837?v=4',
+       details: ['Cursando ', 'CARLOS FERNANDO MARTINEZ SANCHEZ', 'Más información']
     },
 
 ];
