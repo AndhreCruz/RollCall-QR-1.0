b982ec55bf46d23b63407736bb9f47b477811ab1
AndhreCruz <andhrec24@gmail.com>
2024-09-22 23:22:33 -0300

    Agregado el botón de logout en el home y mejoras visuales realizadas

--- src/app/pages/home-user/course-service.service.ts
+++ src/app/pages/home-user/course-service.service.ts
@@ -26,28 +26,28 @@ export class CourseServiceService {
        title: 'Estadistica Descriptiva ',
        imageURL: 'https://cdn-icons-png.flaticon.com/512/432/432796.png',
        profileImg: 'https://learn.content.blackboardcdn.com/3900.100.0-rel.32+b7093a7/images/ci/ng/default_profile_avatar.svg',
-       details: ['Cursando ', 'MONICA NATHALIA PANES MARTINEZ ', ' 100%',' (10 de 10 clases)'],
+       details: [' Cursando ', 'MONICA NATHALIA PANES MARTINEZ ', ' 100%',' (10 de 10 clases)'],
     },
     {
        id: '4',
        title: 'Etica Laboral',
        imageURL: 'https://cdn-icons-png.flaticon.com/512/5776/5776927.png',
        profileImg: 'https://learn.content.blackboardcdn.com/3900.100.0-rel.32+b7093a7/images/ci/ng/default_profile_avatar.svg',
-       details: ['Cursando ', 'LEONARDO OSVALDO MUÑOZ VILLALÓN ', ' 100%',' (6 de 6 clases)'],
+       details: [' Cursando ', 'LEONARDO OSVALDO MUÑOZ VILLALÓN ', ' 100%',' (6 de 6 clases)'],
     },
     {
        id: '5',
-       title: 'Inlges Intermedio',
+       title: 'Ingles Intermedio',
        imageURL: 'https://images.vexels.com/content/201997/preview/english-book-flat-17d09a.png',
        profileImg: 'https://learn.content.blackboardcdn.com/3900.100.0-rel.32+b7093a7/images/ci/ng/default_profile_avatar.svg',
-       details: ['Cursando ', 'JOSE SANTOS JARA FUENTES ', ' 75%',' (15 de 20 clases)'],
+       details: [' Cursando ', 'JOSE SANTOS JARA FUENTES ', ' 75%',' (15 de 20 clases)'],
     },
     {
        id: '6',
        title: 'Programación Aplicaciones Moviles',
        imageURL: 'https://www.svgrepo.com/show/530452/mobile-app.svg',
        profileImg: 'https://avatars.githubusercontent.com/u/8823837?v=4',
-       details: ['Cursando ', 'CARLOS FERNANDO MARTINEZ SANCHEZ', ' 100%',' (9 de 9 clases)']
+       details: [' Cursando ', 'CARLOS FERNANDO MARTINEZ SANCHEZ', ' 100%',' (9 de 9 clases)']
     },
 
 ];
--- src/app/pages/home-user/home-user.page.ts
+++ src/app/pages/home-user/home-user.page.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { Auth } from '@angular/fire/auth';
 import { User } from 'firebase/auth';
 import {CourseServiceService} from './course-service.service'
+import { Router } from '@angular/router';
+import { ToastController } from '@ionic/angular';
 
 
 @Component({
@@ -15,9 +17,12 @@ export class HomeUserPage implements OnInit {
   email: string | null = null;
   courses: any[] = [];
 
-  constructor(private auth: Auth , private courseService: CourseServiceService) {
-
-   }
+  constructor(
+    private router: Router,
+    private toastController: ToastController,
+    private auth: Auth , 
+    private courseService: CourseServiceService,
+  ) {}
 
   ngOnInit() {
     const user: User | null = this.auth.currentUser;
@@ -30,4 +35,18 @@ export class HomeUserPage implements OnInit {
 
   }
 
+  async toastMessage(message: string, color: string) {
+    const toast = await this.toastController.create({
+      message: message,
+      duration: 3000,
+      position: 'bottom',
+      color: color,
+    });
+    toast.present();
+  }
+
+  logout(){
+    this.toastMessage('Se ha cerrado su sesión', 'success');
+    this.router.navigate(['/login']);
+  }
 }
