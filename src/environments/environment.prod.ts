e2810ae5beef79cc57d6cb8a751c4d4039483c06
Andhre <andhrec24@gmail.com>
2024-10-04 16:34:35 -0300

    Agregada la integración de Android y mejoras en la función de cámara

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyA4VMadJh6M9ApSTjgFhFOCEJ1hbeofvNI",
+    authDomain: "validacion-registroapp.firebaseapp.com",
+    projectId: "validacion-registroapp",
+    storageBucket: "validacion-registroapp.appspot.com",
+    messagingSenderId: "851219898010",
+    appId: "1:851219898010:web:9ec24ca990e37738814923"
+  }
 };
+
