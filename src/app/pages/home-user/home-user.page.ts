e8443ce3f436a86c613c54715a17bbec6d97d8d1
NikoBoolean <niko.kacic@gmail.com>
2024-10-27 03:10:53 -0300

    Ultimos cambios al consumo de API de las asignaturas

--- src/app/pages/home-user/home-user.page.ts
+++ src/app/pages/home-user/home-user.page.ts
@@ -5,6 +5,7 @@ import {CourseServiceService} from '../../services/course-service.service'
 import { Router } from '@angular/router';
 import { ToastController } from '@ionic/angular';
 import { LocalStorageService } from '../../services/storage.service';
+import {SignaturesAPIService}  from '../../services/signatures-api.service';
 
 
 @Component({
@@ -17,13 +18,15 @@ export class HomeUserPage implements OnInit {
 
   email: string | null = null;
   courses: any[] = [];
+  signatures: any[]=[];
 
   constructor(
     private router: Router,
     private toastController: ToastController,
     private auth: Auth ,
     private courseService: CourseServiceService,
-    private localStorageService: LocalStorageService
+    private localStorageService: LocalStorageService,
+    private signatureService:SignaturesAPIService
   ) {}
 
   ngOnInit() {
@@ -38,7 +41,12 @@ export class HomeUserPage implements OnInit {
 
     this.courses = this.courseService.getCourses();
 
+    this.signatureService.getSignatures().subscribe((data) => {
+      this.courses = data;
+    });
+
   }
+
 
   async toastMessage(message: string, color: string) {
     const toast = await this.toastController.create({
